132c31df404dd2b11e395e6a6b6d34ba54f4f6eb
allcodebreaker <allwin2436@gmail.com>
2025-04-22 21:19:17 +0530

    Resolved Error in GitHub Link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = ()=>{
                     
                     <span><a href="https://www.instagram.com/allcodebreaker/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i> allCodebreaker </a></span>
                     <span><a href="https://www.linkedin.com/in/allwin-a-023970291/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i> Allwin A </a></span>
-                    <span> <a href="https:/github.com/allcodebreaker/"><i className="fab fa-github"></i> allCodebreaker </a></span>
+                    <span><a href="https://github.com/allCodeBreaker/" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i> allCodebreaker </a></span>
                 </div>      
                 <hr />
                 <p>&copy; {new Date().getFullYear()} Allwin. All rights reserved.</p>            
