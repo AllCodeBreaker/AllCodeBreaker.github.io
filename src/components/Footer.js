57f050b0b7b4c54bdc96af94071f5e05e49032ed
Allwin <allwin2436@gmail.com>
2025-08-12 19:08:26 +0530

    Update Footer.js
    Added  rel="noreferrer"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="socials">
-        <a href="https://github.com/allcodebreaker" target="_blank">GitHub</a>
-        <a href="https://www.linkedin.com/in/allwin-a-023970291/" target="_blank">LinkedIn</a>
-        <a href="https://www.instagram.com/allcodebreaker/" target="_blank">Instagram</a>
+        <a href="https://github.com/allcodebreaker" target="_blank"  rel="noreferrer">GitHub</a>
+        <a href="https://www.linkedin.com/in/allwin-a-023970291/" target="_blank" rel="noreferrer">LinkedIn</a>
+        <a href="https://www.instagram.com/allcodebreaker/" target="_blank" rel="noreferrer">Instagram</a>
         
       </div>
       <p>&copy; {new Date().getFullYear()} Allwin A. All rights reserved.</p>
