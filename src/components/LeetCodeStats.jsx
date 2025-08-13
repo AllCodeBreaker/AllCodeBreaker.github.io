e76d1db0688fccb4072fb1b943a9a3d0bd32d5a4
Allwin <allwin2436@gmail.com>
2025-08-13 10:37:44 +0530

    Create LeetCodeStats.jsx
    Added leetcode stats

---
+++ src/components/LeetCodeStats.jsx
@@ -0,0 +1,14 @@
+const LeetCodeStats = () => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <h2>My LeetCode Stats</h2>
+      <img
+        src="https://leetcard.jacoblin.cool/Allwin_Allwin"
+        alt="LeetCode Stats"
+        style={{ width: '80%', height: 'auto' }}
+      />
+    </div>
+  );
+};
+
+export default LeetCodeStats;
